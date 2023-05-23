6a5c4cc4c13a8ddc0217147c38df900aecbc10fe
Derek Harter <derek@harter.pro>
2023-05-23 10:48:18 -0500

    Second stage in splitting project, move files to top level
    Moved the files for the pxemanage project to top level
    directory of the project now.

--- pxemanage/config.py
+++ pxemanage/config.py
@@ -1,11 +1,18 @@
-#!/usr/bin/env python3
-"""Implement a simple settings from a yaml file mechanism.  This file
-loads configuration settings from an expected location.  Other files can
-use settings by doing
+"""pxemanage module
+
+config submodule
+
+Contents
+--------
+
+Implement a simple settings from a yaml file mechanism.  This file
+loads configuration settings from an expected location.  Other files
+can use settings by doing
 
 from config import settings
 
 print(f"{settings.ks_config_dir}/user-data")
+
 """
 import yaml
 
